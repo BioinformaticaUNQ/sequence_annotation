9cc55b8cdb970b85368b52b11399cf041083105f
Mari Medici <marilau.medici@gmail.com>
2022-07-04 00:16:17 -0300

    mas datos para la proteina

--- src/clients/sifts_client.py
+++ src/clients/sifts_client.py
@@ -8,15 +8,18 @@ class SiftsPDBClient:
         return requests.get(base_url + domain +"/"+ pdb_id).json()
 
     def summary_by_pdb_id(self, pdb_id):
-           return self.__basic_get_call(entry_url,"summary",pdb_id)
+        return self.__basic_get_call(entry_url,"summary",pdb_id)
     
     def molecules_by_pdb_id(self, pdb_id):
-           return self.__basic_get_call(entry_url,"molecules",pdb_id)
+        return self.__basic_get_call(entry_url,"molecules",pdb_id)
     
     def secondary_structure_by_pdb_id(self, pdb_id):
-           return self.__basic_get_call(entry_url,"secondary_structure",pdb_id)
+        return self.__basic_get_call(entry_url,"secondary_structure",pdb_id)
     
     def mappings_by_pdb_id(self,pdb_id):
         return self.__basic_get_call(url,"mappings",pdb_id)
+    
+    def residue_listing_by_pdb_id(self,pdb_id):
+        return self.__basic_get_call(entry_url,"residue_listing",pdb_id)
 
     
--- src/dto/protein.py
+++ src/dto/protein.py
@@ -1,6 +1,8 @@
 class Protein():
     
     
-    def __init__(self, pdb_id, sequence):
+    def __init__(self, pdb_id, sequence, chains):
         self.pdb_id = pdb_id
         self.sequence = sequence
+        self.chains = chains
+        
