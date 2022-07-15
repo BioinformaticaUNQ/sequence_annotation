a386e427e86df0b81a318d17fd7334bb1f3cf34d
Alvaro <alvaropiorno100@gmail.com>
2022-07-14 22:39:17 -0300

    trying test

--- test/clients/modidb_client_test.py
+++ test/clients/modidb_client_test.py
@@ -1,12 +1,17 @@
+import unittest
+import sys
+sys.path.append("..")
 from clients.mobidb_client import MobiDBClient
-from utils.data_sample import query_data_example
+from data_sample import query_data_example
 
-mobidb_client = MobiDBClient()
+class MobidbTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = MobiDBClient()
 
+    def test_retrieves_data_for_a_given_access_id(self):
+        response = self.client.missing_residues(query_data_example["uniprot_id"])
+        self.assertEquals(response["acc"],query_data_example["uniprot_id"])
+        self.assertEquals(response["sequence"],query_data_example["uniprot_sequence"])
 
-def test_retrieves_data_for_a_given_access_id():
-    response = mobidb_client.missing_residues(query_data_example["uniprot_id"])
-    assert response["acc"] == query_data_example["uniprot_id"] \
-           and response["sequence"] == query_data_example["uniprot_sequence"]
-
-
+if __name__ == '__main__':
+    unittest.main()
--- test/services/protein_search_service_test.py
+++ test/services/protein_search_service_test.py
@@ -1,10 +1,17 @@
-from services.proteins_search_service import ProteinsSearchService
-from utils.data_sample import query_data_example
+import unittest
+import sys
+sys.path.append("..")
+from src.services.proteins_search_service import ProteinsSearchService
+from data_sample import query_data_example
 
-protein_search_service = ProteinsSearchService()
+class ProteinSearchServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.service = ProteinsSearchService()
 
+    def test_search_in_all_providers(self):
+        response = self.service.search_in_sites(query_data_example["pdb_id"])
+        self.assertEquals(response["pdb_id"],query_data_example["pdb_id"])
+        self.assertEquals(response["sequence"],query_data_example["pdb_sequence"])
 
-def test_search_in_all_providers():
-    response = protein_search_service.search_in_sites(query_data_example["pdb_id"])
-    assert response["pdb_id"] == query_data_example["pdb_id"] \
-           and response["sequence"] == query_data_example["pdb_sequence"]
+if __name__ == '__main__':
+    unittest.main()
