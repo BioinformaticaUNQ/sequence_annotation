7d140c2523db09ac15c13f86c10958087dcf880c
Angelo Padron <padron891@gmail.com>
2022-07-18 18:30:21 -0300

    added summary and uniprot accessions in output, and other minor changes

--- src/dto/protein.py
+++ src/dto/protein.py
@@ -1,7 +1,9 @@
 class Protein:
 
-    def __init__(self, pdb_id, sequence, chains, missing_residues):
+    def __init__(self, pdb_id, related_uniprot_accessions, summary, pdb_sequence, chains, missing_residues=None):
         self.pdb_id = pdb_id
-        self.sequence = sequence
+        self.related_uniprot_accessions = related_uniprot_accessions
+        self.summary = summary
+        self.pdb_sequence = pdb_sequence
         self.chains = chains
         self.missing_residues = missing_residues
